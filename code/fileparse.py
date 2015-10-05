5b94f52a058c7067469cf396ade35e1d5c00bb0f
vaibhavkumar <vaibhav4595@gmail.com>
2015-10-06 02:17:04 +0530

    Feature defs added

--- code/fileparse.py
+++ code/fileparse.py
@@ -1,5 +1,20 @@
+import token_gen
+import nltk
+import enchant
+
+Dict = enchant.Dict("en_US")
+
 class files:
+
 	def __init__(self, arr):
+		"""
+		Initiliases an object out of given features
+		===========================================
+		Parameters :
+			* array
+				- contains set of features
+		===========================================
+		"""
 		self.essay_id = arr[0]
 		self.essay_set = arr[1]
 		self.essay = arr[2]
@@ -29,15 +44,119 @@ class files:
 		self.r3_t5 = arr[26]
 		self.r3_t6 = arr[27]
 
+		self.token_arr = token_gen.tokenize([self.essay])
+
+	def set_word_count(self, min_len):
+		"""
+		Sets different count (of words) / spelling related features
+		================================================
+		Sets the following:
+			* sets the word count in total
+			* sets the long word count
+			* sets the average word lenth
+			* sets the spelling errors
+		================================================
+		"""
+
+		self.word_count = len(self.token_arr)
+
+		self.dic = {}
+		self.long_word_count = 0
+		self.avg_word_len = 0
+		self.lex_diversity = 0
+		self.spell_errors = 0
+
+		for each in self.token_arr:
+			if len(each) > min_len:
+				self.long_word_count += 1
+			if not self.dic.has_key(each):
+				self.dic[each] = 1
+				self.lex_diversity += 1
+			if len(each) > 1:
+				try:
+					if not Dict.check(each.encode('utf8')):
+						self.spell_errors += 1
+				except:
+					self.spell_errors += 1
+			self.avg_word_len += len(each)
+
+		self.avg_word_len = float(self.avg_word_len) / float(self.word_count)
+
+	def set_pos_features(self):
+		"""
+		Sets tag related features
+		===============================================
+		Sets different features like:
+			* Nount count
+			* Verb count
+			* Adjective count
+			* Adverb count
+		===============================================
+		"""
+
+		self.pos_tags = nltk.pos_tag(self.token_arr)
+		
+		self.verb_count = 0
+		self.noun_count = 0
+		self.adj_count = 0
+		self.adv_count = 0
+
+		for each in self.pos_tags:
+			if each[1].startswith('NN'):
+				self.noun_count += 1
+			elif each[1].startswith('JJ'):
+				self.adj_count += 1
+			elif each[1].startswith('RB'):
+				self.adv_count += 1
+			elif each[1].startswith('VB'):
+				self.verb_count += 1
+
+
+	def set_punctuation_features(self):
+		"""
+		Sets different featured realted to punctuations
+		===============================================
+		Sets different features like:
+			* Comma count
+			* Quotation mark count
+			* Punctuation count
+			* Sentence count
+		===============================================
+		"""
+
+		self.comma_count = 0
+		self.sen_count = 0
+		self.punc_count = 0
+		self.quo_count = 0
+
 
 def get_list():
-	fo = open('../data/training_set.tsv')
+	"""
+	Creates an array of objects out of 
+	input training file
+	==================================
+	Returns:
+		* array of objects where each
+		object corresponds to a document
+	==================================
+	"""
+
+	fo = open('../data/code_test.tsv')
 	lines = fo.readlines()
 	fo.close()
 
 	obj_arr = []
 	for each in lines:
 		send_obj = files(each.split('\n')[0].split('\t'))
+		send_obj.set_word_count(5)
+		send_obj.set_pos_features()
+		send_obj.set_pos_features()
+		send_obj.set_punctuation_features()
 		obj_arr.append(send_obj)
 
 	return obj_arr
+
+if __name__ == "__main__":
+	arr = get_list()
+	print arr[0].pos_tags
+	print arr[0].verb_count
