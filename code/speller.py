1c107628f2b1d02b8fbb202a5c40e49b90997882
Mrinal Dhar <mrinal.dhar@gmail.com>
2015-10-05 23:22:55 +0530

    Spell checker: bugfix

--- code/speller.py
+++ code/speller.py
@@ -5,14 +5,14 @@ misspelled words given an essay.
 
 '''
 
-import nltk
 import enchant
+import token_gen
 
 Dict = enchant.Dict("en_US")
 
 def num_spell_errors(essay):
 	errors = 0
-	tokens = nltk.tokenize(essay.essay)
+	tokens = token_gen(essay.essay)
 	for each in tokens:
 		if not Dict.check(each):
 			errors += 1
