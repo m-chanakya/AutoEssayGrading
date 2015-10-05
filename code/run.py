54dc15fdb31b0f8ff0f53b15ad27e501e9300ba1
Mrinal Dhar <mrinal.dhar@gmail.com>
2015-10-06 02:21:22 +0530

    Modified runner script

--- code/run.py
+++ code/run.py
@@ -5,9 +5,6 @@ Main script that runs everything.
 '''
 
 import fileparse as docR
-import speller
 
 if __name__=="__main__":
 	docs_list = docR.get_list()
-	for doc in docs_list:
-		speller.num_spell_errors(doc)
