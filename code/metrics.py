10430526d8b85d92fbecf6233e870663639d2ab2
vaibhavkumar <vaibhav4595@gmail.com>
2015-10-06 14:54:54 +0530

    Phase 1 complete

---
+++ code/metrics.py
@@ -0,0 +1,20 @@
+import skll.metrics as met
+
+def get_average_kappa(arr_act, arr_pred):
+	"""
+	Calculates the average quadratic kappa
+	over the entire essay set
+	"""
+
+	assert(len(arr_act) == len(arr_pred))
+	total = len(arr_act)
+	kappa_val = 0
+
+	for i in xrange(0, total):
+		kappa_val += met.kappa([arr_act[i]], [arr_pred[i]], \
+					'quadratic')
+		print arr_act[i], '-', arr_pred[i]
+
+	kappa_val  = float(kappa_val) / float(total)
+
+	return kappa_val
--- code/run.py
+++ code/run.py
@@ -10,13 +10,30 @@ from sklearn.externals import joblib
 import pickle 
 import sys
 import numpy as np
+from metrics import get_average_kappa as kappa
+
+def begin_testing(filename, classifier):
+	test_docs = docR.get_list(filename)
+	data = []
+	target = []
+	for doc in test_docs:
+		data.append(doc.vector[:-1])
+		target.append(doc.vector[-1])
+	np_data = np.array(data)
+	np_target = np.array(target)
+	results = classifier.predict(np_data)
+	kp = kappa(np_target, results)
+	print "\nThe Average Quadratic Weighted Kappa obtained is: ", kp, "\n"
+	print "="*50
 
 if __name__=="__main__":
-	if len(sys.argv) < 4:
-		print "USAGE: $ python run.py [-n | -o] model_file data_file"
-
+	if len(sys.argv) < 5:
+		print "USAGE: $ python run.py [-n | -o] input_file model_file data_file -t test_file"
+		sys.exit(0)
+	print "\n"
+	print "="*50
 	if sys.argv[1] == "-n":
-		docs_list = docR.get_list()
+		docs_list = docR.get_list(sys.argv[2])
 		classifier = svm.SVR()
 		data = []
 		target = []
@@ -26,20 +43,22 @@ if __name__=="__main__":
 		np_data = np.array(data)
 		np_target = np.array(target)
 		classifier.fit(np_data, np_target)
-		joblib.dump(classifier, sys.argv[2])
+		joblib.dump(classifier, sys.argv[3])
 		save_data = data
 		save_data.append(target)
 		string = pickle.dumps(save_data)
-		ofp = open(sys.argv[3], 'w')
+		ofp = open(sys.argv[4], 'w')
 		ofp.write(string)
 		ofp.close()
 		print "Model and data dumped successfully."
 
 	elif sys.argv[1] == "-o":
-		classifier = joblib.load(sys.argv[2])
-		saved_data = open(sys.argv[3], 'r')
+		classifier = joblib.load(sys.argv[3])
+		saved_data = open(sys.argv[4], 'r')
 		saved_data = saved_data.read()
 		saved_data = pickle.loads(saved_data)
 		data = np.array(saved_data[:-1])
-		target = np.array(saved_data[-1])
+		target = np.array([saved_data[-1]])
 		print "Model and data loaded successfully."
+	if sys.argv[5] == '-t':	
+		begin_testing(sys.argv[6], classifier)	
